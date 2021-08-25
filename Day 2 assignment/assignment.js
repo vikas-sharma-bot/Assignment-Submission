c90e8b2b153ba5c765b0c914a6fc0b46f834f4b0
Vikas Sharma <67753092+vikas-sharma-bot@users.noreply.github.com>
2021-08-25 17:59:49 +0530

    Assignment submit

---
+++ Day 2 assignment/assignment.js
@@ -0,0 +1,29 @@
+let food = [
+    {
+        name: "Pasta",
+        price: 30,
+        quantity: 2
+    },
+    {
+        name: "Burgur",
+        price: 50,
+        quantity: 2
+    },
+    {
+        name: "Pizza",
+        price: 150,
+        quantity: 1
+    }, 
+    {
+        name: "Momos",
+        price: 200,
+        quantity:4
+    },
+]
+
+
+
+for (let num = 0; num < food.length; num++) {
+    console.log(food[num]);
+    
+}
